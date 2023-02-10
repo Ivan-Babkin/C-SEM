94f530e522142b89fe46eeb3b78f67eeee652e28
Ivan Babkin <isbabkin@mail.ru>
2023-02-10 21:14:38 +0500

    New tasks

---
+++ Sem01/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine(" ");
+Console.WriteLine("Home Work 01");
+
+Console.WriteLine(" ");
+Console.WriteLine("The 1st numbers");
+double a1, b1;
+        Console.Write("Введите первое число a1: ");
+        a1 = Double.Parse(Console.ReadLine());
+        Console.Write("Введите второе число b1: ");
+        b1 = Double.Parse(Console.ReadLine());
+
+if (a1 > b1)
+   Console.WriteLine("{0} - максимальное значение, {1} - минимальное значение", a1, b1);
+else Console.WriteLine("{0} - максимальное значение, {1} - минимальное значение", b1, a1);
+
+Console.WriteLine(" ");
+Console.WriteLine("The 2nd numbers");
+double a2, b2;
+        Console.Write("Введите первое число a2: ");
+        a2 = Double.Parse(Console.ReadLine());
+        Console.Write("Введите второе число b2: ");
+        b2 = Double.Parse(Console.ReadLine());
+
+if (a2 > b2)
+   Console.WriteLine("{0} - максимальное значение, {1} - минимальное значение", a2, b2);
+else Console.WriteLine("{0} - максимальное значение, {1} - минимальное значение", b2, a2);
+
+Console.WriteLine(" ");
+Console.WriteLine("The 3rd numbers");
+double a3, b3;
+        Console.Write("Введите первое число a3: ");
+        a3 = Double.Parse(Console.ReadLine());
+        Console.Write("Введите второе число b3: ");
+        b3 = Double.Parse(Console.ReadLine());
+
+if (a3 > b3)
+   Console.WriteLine("{0} - максимальное значение, {1} - минимальное значение", a3, b3);
+else Console.WriteLine("{0} - максимальное значение, {1} - минимальное значение", b3, a3);
