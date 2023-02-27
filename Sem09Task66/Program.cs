eb841e591b9d0ae8b04fbe970347c9d08dbf506f
Ivan-Babkin <125073447+Ivan-Babkin@users.noreply.github.com>
2023-02-27 21:37:01 +0500

    Added files for Seminar 09 Tasks 64, 66 and 68

---
+++ Sem09Task66/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 66
+// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine($" ");
+Console.Write("Уважаемый, введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("А теперь, введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+SumFromMToN(m, n);
+
+// Вызов функции "сумма чисел от M до N"
+void SumFromMToN(int m, int n)
+{
+    Console.Write($"Сумма натуральных чисел будет: ");
+    Console.Write(SumMN(m - 1, n));    
+}
+
+// Функция суммы чисел от M до N
+int SumMN(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + SumMN(m, n);
+        return res;
+    }
+}
